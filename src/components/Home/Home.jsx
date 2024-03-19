a30c4fe0f79e8601bd0253c7ce0739e7a4f07233
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-19 15:51:59 +0530

    play3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 import homeKid from '../../assets/homekid1.jpg'
 
 import './Home.css'
+import Home2half from './Home2half'
+
 
 const Home = () => {
   return (
+    <>
     <div className="home overflow-hidden px-20 py-20 -mt-5 w-full fill-yellow-400 max-md:px-5 max-md:max-w-full">
     <div className=" flex gap-5 max-md:flex-col max-md:gap-0">
       <div className="flex flex-col w-[45%] max-md:ml-0 max-md:w-full">
         <div className="flex flex-col self-stretch my-auto text-6xl text-fuchsia-900 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-          <div className="z-10 tracking-widest border-white border-solid border-[3px] max-md:max-w-full max-md:text-4xl">
+          {/* <div className="z-10 tracking-widest border-white border-solid border-[3px] max-md:max-w-full max-md:text-4xl">
             Welcome to Kiddy Rainbow Slime CO.
-          </div>
+          </div> */}
           <div className="tracking-widest border-fuchsia-900 border-solid border-[3px] max-md:max-w-full max-md:text-4xl">
             Welcome to Kiddy Rainbow Slime CO.
           </div>
@@ -69,6 +72,14 @@ const Home = () => {
       </div>
     </div>
   </div>
+<Home2half/>
+
+</>
+
+
+
+
+
   )
 }
 
---
+++ src/components/Home/Home2half.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const Home2half = () => {
+  return (
+<>
+<div className="self-center mt-24 text-4xl tracking-wider text-center text-fuchsia-900 w-[521px] max-md:mt-10 max-md:max-w-full mx-auto">
+    Providing Good Qualities For Your Loving Kids
+</div>
+
+
+    <div className="self-center px-5 mt-11 w-full max-w-[1272px] max-md:mt-10 max-md:max-w-full">
+      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+        <div className="flex flex-col w-3/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow items-center px-11 pt-5 pb-8 mt-2 w-full text-center text-fuchsia-900 bg-pink-100 rounded-xl border-2 border-solid border-zinc-400 max-md:px-5 max-md:mt-8">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="aspect-square w-[65px]"
+            />
+            <div className="mt-5 text-2xl font-semibold tracking-wider">
+              Bus Service
+            </div>
+            <div className="self-stretch mt-5 text-base tracking-wide">
+              ubiquitous models rather than parallel initiatives. Seamlessly
+              reinvent success.
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow px-11 pt-5 pb-8 mt-2 w-full text-center text-white rounded-xl border-2 border-solid bg-fuchsia-950 border-fuchsia-950 max-md:px-5 max-md:mt-8">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="self-center aspect-square w-[65px]"
+            />
+            <div className="mt-5 text-2xl font-semibold tracking-wider">
+              Sports Training
+            </div>
+            <div className="mt-3.5 text-base tracking-wide">
+              ubiquitous models rather than parallel initiatives. Seamlessly
+              reinvent success.
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow px-11 pt-5 pb-8 mt-2 w-full text-center text-fuchsia-900 bg-pink-100 rounded-xl border-2 border-solid border-zinc-400 max-md:px-5 max-md:mt-8">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="self-center aspect-square w-[65px]"
+            />
+            <div className="mt-5 text-2xl font-semibold tracking-wider">
+              Music Training
+            </div>
+            <div className="mt-3.5 text-base tracking-wide">
+              ubiquitous models rather than parallel initiatives. Seamlessly
+              reinvent success.
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col ml-5 w-3/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col items-center px-11 pt-5 pb-8 w-full text-center text-fuchsia-900 bg-pink-100 rounded-xl border-2 border-solid border-zinc-400 max-md:px-5 max-md:mt-6">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="aspect-square w-[65px]"
+            />
+            <div className="mt-5 text-2xl font-semibold tracking-wider">
+              Best Teachers
+            </div>
+            <div className="self-stretch mt-5 text-base tracking-wide">
+              ubiquitous models rather than parallel initiatives. Seamlessly
+              reinvent success.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Home2half
