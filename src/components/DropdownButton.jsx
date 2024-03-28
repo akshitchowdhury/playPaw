f54ac024e9dd8c24663aed4d37e54841c741a504
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-03-28 13:05:24 +0530

    recreate101

--- src/components/DropdownButton.jsx
+++ src/components/DropdownButton.jsx
@@ -12,9 +12,9 @@ import './Nav.css'
 export default function DropdownBasicExample() {
   return (
     <TEDropdown className="flex justify-center">
-      <TERipple rippleColor="light">
+      <TERipple rippleColor="dark">
         <TEDropdownToggle
-          className= "dropdown flex items-center whitespace-nowrap rounded"
+          className= "dropdown flex items-center whitespace-nowrap rounded "
         >
           Event Parties
           <span className="ml-2 [&>svg]:w-5 w-2">
@@ -37,7 +37,7 @@ export default function DropdownBasicExample() {
         <TEDropdownItem>
           <a
             href="#"
-            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
+            className="block w-full  min-w-[160px] cursor-pointer whitespace-nowrap bg-pink-400 px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
           >
             Birthday Celebrations
           </a>
@@ -45,7 +45,7 @@ export default function DropdownBasicExample() {
         <TEDropdownItem>
           <a
             href="#"
-            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
+            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-pink-400 px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
           >
             Kitty Parties
           </a>
@@ -53,7 +53,7 @@ export default function DropdownBasicExample() {
         <TEDropdownItem>
           <a
             href="#"
-            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
+            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-pink-400 px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
           >
             Event Parties
           </a>
@@ -61,7 +61,7 @@ export default function DropdownBasicExample() {
         <TEDropdownItem>
           <a
             href="#"
-            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
+            className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-pink-400 px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600"
           >
             Cafe
           </a>
