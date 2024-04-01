badcfebe80a792357d8edec105617a815988d379
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-01 18:26:48 +0530

    finalize8

--- src/components/Pricing/PricingComponent.jsx
+++ src/components/Pricing/PricingComponent.jsx
@@ -215,7 +215,8 @@ const PricingComponent = () => {
         <br />        
         <br />        
         <br />        
-        
+
+                      {/* /* Membership rates */ }
         
         <h1 className="bubblegum-sans-regular tracking-widest border-fuchsia-900 text-fuchsia-900">Membership Rates</h1>
         <br />
@@ -556,6 +557,218 @@ const PricingComponent = () => {
             </Link>
           </div>
         </div>
+        <br />
+        <br />
+
+                                          {/* /* Special offers */ }
+
+
+    <h1 className="bubblegum-sans-regular tracking-widest border-fuchsia-900 text-fuchsia-900">We've also got something Special for you</h1>
+      <br />
+      <br />
+      <div className="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0">
+          {/* Pricing Card */}
+          
+          {/* Additional Pricing Cards */}
+
+          <div className="special1 flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100  xl:p-8 dark:bg-gray-800 dark:text-white">
+            <h3 className="mb-4 text-2xl font-semibold">Party Hall: Groove Time</h3>
+            {/* <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p> */}
+            <div className="flex justify-center items-baseline my-8">
+              <span className="mr-2 text-3xl font-extrabold">Rs. 12000</span>
+              <span className="text-gray-500 dark:text-gray-400">Party Hall only</span>
+            </div>
+            {/* List */}
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Unlimited access to all play zones</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Rs.200/kid entry</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Kids only!</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Weekend charges may vary</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Socks Mandatory</span>
+              </li>
+              {/* Additional list items */}
+            </ul>
+
+            <Link to='/contact'>
+            <button className="bg-pink-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 text-white font-medium rounded-full text-sm px-5 py-2.5 text-center">
+              Book now
+            </button></Link>
+          </div>
+
+          <div className="special2 flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100  xl:p-8 dark:bg-gray-800 dark:text-white">
+            <h3 className="mb-4 text-2xl font-semibold">Pawful combo: The Party won't stop</h3>
+            {/* <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p> */}
+            <div className="flex justify-center items-baseline my-8">
+              <span className="mr-2 text-3xl font-extrabold">Rs. 12000</span>
+              <span className="text-gray-500 dark:text-gray-400">Party Hall+ Play Area</span>
+            </div>
+            {/* List */}
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Unlimited access to all play zones</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Rs.200/kids entry</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Kids only</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Weekend charges may vary</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                {/* Icon */}
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Socks Mandatory</span>
+              </li>
+              {/* Additional list items */}
+            </ul>
+            <Link to='/contact'>
+            <button className="bg-pink-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 text-white font-medium rounded-full text-sm px-5 py-2.5 text-center">
+            Book now
+            </button>
+            </Link>
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
