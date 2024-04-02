8c039945c009744ae95b9f1c27b4af2a452064d8
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-02 16:35:19 +0530

    discountAddition13

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,9 +25,9 @@ const About = () => {
   <p className="paraText bubblegum-sans-regular text-lg mb-2 md:mb-4">
     At Play Pawws, we believe in creating a world of fun and excitement for kids! Our playzone area is designed to be a haven of joy, where children can unleash their imagination and energy in a safe and vibrant environment.
     <br/><br/>
-    With colorful play structures, interactive games, and engaging activities, Play Paw is the ultimate destination for kids to laugh, play, and make new friends. Our dedicated staff ensures that every visit is filled with unforgettable moments and endless smiles.
+    With colorful play structures, interactive games, and engaging activities, Play Pawws is the ultimate destination for kids to laugh, play, and make new friends. Our dedicated staff ensures that every visit is filled with unforgettable moments and endless smiles.
     <br/><br/>
-    Whether it's a birthday party, a family outing, or just a day of fun, Play Paw is the perfect place for kids to explore, learn, and grow. Join us today and let the adventure begin!
+    Whether it's a birthday party, a family outing, or just a day of fun, Play Pawws is the perfect place for kids to explore, learn, and grow. Join us today and let the adventure begin!
   </p>
 </div>
 
