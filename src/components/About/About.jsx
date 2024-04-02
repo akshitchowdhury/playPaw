5adbc3d2e70b0b4d8dd8860fb3ff6871b6b80d02
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-02 16:34:35 +0530

    discountAddition11

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
           <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-start" style={{ padding: '4%' }}>
   <img src={about} alt="placeholder" className="eventImg md:w-full" style={{ height: '400px', borderRadius: '5%', marginRight: '4%', width:'100%' }} />
   <p className="paraText bubblegum-sans-regular text-lg mb-2 md:mb-4">
-    At Play Paw, we believe in creating a world of fun and excitement for kids! Our playzone area is designed to be a haven of joy, where children can unleash their imagination and energy in a safe and vibrant environment.
+    At Play Pawws, we believe in creating a world of fun and excitement for kids! Our playzone area is designed to be a haven of joy, where children can unleash their imagination and energy in a safe and vibrant environment.
     <br/><br/>
     With colorful play structures, interactive games, and engaging activities, Play Paw is the ultimate destination for kids to laugh, play, and make new friends. Our dedicated staff ensures that every visit is filled with unforgettable moments and endless smiles.
     <br/><br/>
